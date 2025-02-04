24944d03bdf8943e419bc2350fb3aaf97120eef8
AmirH-Hemati <amirhosseinhemat9@gmail.com>
2025-02-05 00:39:14 +0330

    create custom theme in tailwind  for shadow

--- client/src/ui/Header.jsx
+++ client/src/ui/Header.jsx
@@ -1,6 +1,6 @@
 function Header() {
   return (
-    <aside className="w-[20%] h-full bg-[#192938] [box-shadow:-5px_-5px_15px_hsla(0,0%,100%,.1),5px_5px_15px_rgba(0,0,0,.35)] rounded-sm">
+    <aside className="w-[20%] h-full bg-[#192938] shadow-custom rounded-sm">
       {" "}
       header
     </aside>
--- client/src/ui/Layout.jsx
+++ client/src/ui/Layout.jsx
@@ -5,7 +5,7 @@ function Layout() {
   return (
     <div className="w-full bg-[#192938] h-screen flex p-4 gap-4 text-white">
       <Header />
-      <main className="w-[80%] bg-[#192938] h-full rounded-sm [box-shadow:-5px_-5px_15px_hsla(0,0%,100%,.1),5px_5px_15px_rgba(0,0,0,.35)]">
+      <main className="w-[80%] bg-[#192938] h-full rounded-sm shadow-custom">
         <Outlet />
       </main>
     </div>
