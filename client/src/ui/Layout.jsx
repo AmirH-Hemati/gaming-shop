3ea4a535ac673c1c9ce4a28d3338553c9460f4f1
AmirH-Hemati <amirhosseinhemat9@gmail.com>
2025-02-05 00:42:33 +0330

    install icon sax for use icon in project

--- client/src/ui/Layout.jsx
+++ client/src/ui/Layout.jsx
@@ -6,6 +6,9 @@ function Layout() {
     <div className="w-full bg-[#192938] h-screen flex p-4 gap-4 text-white">
       <Header />
       <main className="w-[80%] bg-[#192938] h-full rounded-sm shadow-custom">
+        <div>
+          
+        </div>
         <Outlet />
       </main>
     </div>
