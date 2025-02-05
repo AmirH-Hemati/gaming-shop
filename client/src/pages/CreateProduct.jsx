903ae035f5b2f127f908b16999738ea6aa555a70
AmirH-Hemati <amirhosseinhemat9@gmail.com>
2025-02-05 20:16:55 +0330

    show erro when input empty

--- client/src/pages/CreateProduct.jsx
+++ client/src/pages/CreateProduct.jsx
@@ -1,9 +1,14 @@
+import { toast } from "react-toastify";
 import { useCreateProduct } from "../features/products/useCreateProduct";
 
 function CreateProduct() {
   const { createProduct } = useCreateProduct();
   function handelCreateProduct(e) {
     e.preventDefault();
+    const title = e.target.title.value;
+    const price = e.target.price.value;
+    const file = e.target.file.files[0];
+    if (!title || !price || !file) return toast.error("input empty");
     const formData = new FormData(e.target);
     createProduct(formData);
   }
