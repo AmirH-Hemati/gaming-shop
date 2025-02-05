bd8144378344d067634b0eef71ee0623eed4530a
AmirH-Hemati <amirhosseinhemat9@gmail.com>
2025-02-05 18:00:33 +0330

    add navigate when user login

--- client/src/features/authorization/useLogin.js
+++ client/src/features/authorization/useLogin.js
@@ -1,8 +1,10 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { login as loginAPI } from "../../services/apiAuth";
 import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
 
 export function useLogin() {
+  const navigate = useNavigate();
   const queryClient = useQueryClient();
   const { mutate: login, isLoading } = useMutation({
     mutationFn: ({ email, password }) => loginAPI({ email, password }),
@@ -11,6 +13,7 @@ export function useLogin() {
         queryKey: ["user"],
       });
       toast.success("Welcome !...");
+      navigate("/");
     },
     onError: (err) => {
       toast.error(err.response.data.message);
