5a4534cf37b552b7aaf03fb4f963175ef9aaa38b
AmirH-Hemati <amirhosseinhemat9@gmail.com>
2025-02-05 18:32:35 +0330

    config multer for get data in client

--- server/routes/product.js
+++ server/routes/product.js
@@ -1,8 +1,19 @@
 import express from "express";
+import multer from "multer";
+import path from "path";
 import { createProduct, products } from "../controllers/productControllers.js";
 const router = express.Router();
 
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + path.extname(file.originalname));
+  },
+});
+const upload = multer({ storage: storage });
 router.get("/", products);
-router.post("/createProducte", createProduct);
+router.post("/createProducte", upload.single("file"), createProduct);
 
 export default router;
