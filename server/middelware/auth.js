b5589ddd05fcb4b8ebb3e7a77fa2994bdc5dc990
AmirH-Hemati <amirhosseinhemat9@gmail.com>
2025-02-05 18:17:37 +0330

    create middelware for detected user

---
+++ server/middelware/auth.js
@@ -0,0 +1,20 @@
+import data from "../../client/src/data/navListData";
+import User from "../models/auth.js";
+import jwt from "jsonwebtoken";
+export async function auth(req, res, next) {
+  const token = req.header["authorization"];
+  if (!token) {
+    return res.status(401).json({ message: "Acsess Deniged", data: null });
+  }
+  try {
+    const decode = jwt.verify(token, "asddwqdwq456dwqdwq");
+    const user = await User.findOne({ _id: decode.id });
+    if (!user) {
+      return res.status(401).json({ message: "acsess deniged", data: null });
+    }
+    req.user = user;
+    next();
+  } catch (error) {
+    res.staus(402).json({ message: "inValid Token", data: null });
+  }
+}
