91a0a971e96c33666206cca6e38c1f8d5ae83fe9
AmirH-Hemati <amirhosseinhemat9@gmail.com>
2025-02-05 23:23:52 +0330

    create custom hook for get information user in api

---
+++ client/src/features/authorization/useCurrentUse.js
@@ -0,0 +1,9 @@
+import { useQuery } from "@tanstack/react-query";
+import { getCurrentUser } from "../../services/apiAuth";
+export function useCurrentUser() {
+  const { data: user, isLoading } = useQuery({
+    queryKey: ["user"],
+    queryFn: getCurrentUser,
+  });
+  return { user, isLoading };
+}
--- client/src/services/apiAuth.js
+++ client/src/services/apiAuth.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import axiosInstance from "./axiosInstance";
 export async function login({ email, password }) {
   const { data } = await axios.post("http://192.168.1.4:1212/api/auth/login", {
     email,
@@ -8,10 +9,17 @@ export async function login({ email, password }) {
 }
 
 export async function register({ userName, email, password }) {
-  const { data } = await axios.post("http://192.168.1.4:1212/api/auth/register", {
-    userName,
-    email,
-    password,
-  });
+  const { data } = await axios.post(
+    "http://192.168.1.4:1212/api/auth/register",
+    {
+      userName,
+      email,
+      password,
+    }
+  );
+  return data;
+}
+export async function getCurrentUser() {
+  const { data } = await axiosInstance.get("/user/me");
   return data;
 }
