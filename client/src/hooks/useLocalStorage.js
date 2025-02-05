e0a393f34921e4122bdbcbfebf28d29a41a77cc1
AmirH-Hemati <amirhosseinhemat9@gmail.com>
2025-02-05 21:10:12 +0330

    create custom hook local storage for store data in local storage

---
+++ client/src/hooks/useLocalStorage.js
@@ -0,0 +1,13 @@
+import { useEffect, useState } from "react";
+
+export function useLocalStorage(key, initailValue) {
+  const [value, setValue] = useState(() => {
+    const storedValue = localStorage.getItem(key);
+    return storedValue ? JSON.parse(storedValue) : initailValue;
+  });
+  useEffect(() => {
+    localStorage.setItem(key, JSON.stringify(value));
+  }, [key, value]);
+
+  return [value, setValue];
+}
