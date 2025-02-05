9b20d01a5ab42773d738db5d1f07f37f58fb73e4
AmirH-Hemati <amirhosseinhemat9@gmail.com>
2025-02-05 12:43:42 +0330

    add config query client

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,15 +2,18 @@ import { Route, Routes } from "react-router-dom";
 import Layout from "./ui/Layout";
 import Home from "./pages/Home";
 import AuthPage from "./pages/AuthPage";
-
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+const queryClient = new QueryClient();
 function App() {
   return (
-    <Routes>
-      <Route element={<Layout />}>
-        <Route index element={<Home />} />
-        <Route path="/auth" element={<AuthPage />} />
-      </Route>
-    </Routes>
+    <QueryClientProvider client={queryClient}>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="/auth" element={<AuthPage />} />
+        </Route>
+      </Routes>
+    </QueryClientProvider>
   );
 }
 
