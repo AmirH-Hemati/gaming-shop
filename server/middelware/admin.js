e797b06ab4af76d2336be06eb76be3fe530abd5d
AmirH-Hemati <amirhosseinhemat9@gmail.com>
2025-02-05 18:20:22 +0330

    not select password in auth middelware

--- server/middelware/admin.js
+++ server/middelware/admin.js
@@ -1,4 +1,4 @@
-export async function admin(req, res, next) {
+export function admin(req, res, next) {
   if (!req.user || req.user.role !== "admin") {
     return res.status(401).json({ message: "Acsess Deniged", data: null });
   }
