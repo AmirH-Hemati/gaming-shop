6808931dcbf049590ee2f40f2c0859d41c1d0803
AmirH-Hemati <amirhosseinhemat9@gmail.com>
2025-02-06 10:27:54 +0330

    create compponenet for top menu

--- client/src/ui/Layout.jsx
+++ client/src/ui/Layout.jsx
@@ -1,14 +1,10 @@
-import { NavLink, Outlet } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import Header from "./Header";
-import { HambergerMenu, Heart, ShoppingCart } from "iconsax-react";
 import { useState } from "react";
-import { useAddToCart } from "../context/ShoppingContext";
-import { useAddToFavorites } from "../context/FavorietsContext";
 import { useCurrentUser } from "../features/authorization/useCurrentUse";
+import TopMain from "./TopMain";
 
 function Layout() {
-  const { totalQty } = useAddToCart();
-  const { totalFavorites } = useAddToFavorites();
   const [isActiveMenu, setIsActiveMenu] = useState(false);
   const { user } = useCurrentUser();
   console.log(user);
@@ -16,58 +12,14 @@ function Layout() {
     <div className="w-full bg-[#192938] h-screen overflow-hidden flex p-4 gap-4 text-white">
       <Header isActiveMenu={isActiveMenu} />
       <main
-        className={` w-full relative overflow-auto ${
+        className={` w-full md:w-[80%] relative overflow-auto ${
           isActiveMenu ? "md:w-[92%] " : "md:w-[80%] "
         }bg-[#192938] h-full rounded-sm shadow-custom p-4`}
       >
-        <div
-          className={`w-full flex  justify-between duration-500  ${
-            isActiveMenu
-              ? "translate-x-4 px-4 md:px-0 md:translate-x-0"
-              : "translate-x-0"
-          } `}
-        >
-          <HambergerMenu
-            size="32"
-            color="white"
-            onClick={() => setIsActiveMenu(!isActiveMenu)}
-          />
-
-          <div className="flex gap-2 md:gap-4 items-center">
-            <div className="relative">
-              <Heart size="26" color="white" className="cursor-pointer" />
-              <p className="text-sm flex items-center justify-center w-4 h-4 absolute -bottom-1 -right-1 bg-[#0998a8] p-1 rounded-full ">
-                {totalFavorites()}
-              </p>
-            </div>
-            <div className="relative">
-              <ShoppingCart
-                size="26"
-                color="white"
-                className="cursor-pointer"
-              />
-              <p className="text-sm flex items-center justify-center w-4 h-4 absolute -bottom-1 -right-1 bg-[#0998a8] p-1 rounded-full ">
-                {totalQty()}
-              </p>
-            </div>
-            <div>
-              {user?.data ? (
-                <div className="flex gap-2 items-center shadow-custom px-4 py-1">
-                  <p>{user?.data?.userName}</p>
-                  <img src={user?.data?.avatar} className="w-7 h-7 rounded-full" />
-                </div>
-              ) : (
-                <NavLink
-                  to={`/auth`}
-                  className={`py-0.5 px-3 bg-[#0998a8] rounded-sm`}
-                >
-                  login
-                </NavLink>
-              )}
-            </div>
-          </div>
-        </div>
-
+        <TopMain
+          isActiveMenu={isActiveMenu}
+          setIsActiveMenu={setIsActiveMenu}
+        />
         <Outlet />
       </main>
     </div>
---
+++ client/src/ui/TopMain.jsx
@@ -0,0 +1,58 @@
+import { HambergerMenu, Heart, ShoppingCart } from "iconsax-react";
+import { NavLink } from "react-router-dom";
+import { useCurrentUser } from "../features/authorization/useCurrentUse";
+import { useAddToCart } from "../context/ShoppingContext";
+import { useAddToFavorites } from "../context/FavorietsContext";
+
+function TopMain({ isActiveMenu, setIsActiveMenu }) {
+  const { user } = useCurrentUser();
+  const { totalQty } = useAddToCart();
+  const { totalFavorites } = useAddToFavorites();
+  return (
+    <div
+      className={`w-full flex  justify-between duration-500  ${
+        isActiveMenu
+          ? "translate-x-4 px-4 md:px-0 md:translate-x-0"
+          : "translate-x-0"
+      } `}
+    >
+      <HambergerMenu
+        size="32"
+        color="white"
+        onClick={() => setIsActiveMenu(!isActiveMenu)}
+      />
+
+      <div className="flex gap-2 md:gap-4 items-center">
+        <div className="relative">
+          <Heart size="26" color="white" className="cursor-pointer" />
+          <p className="text-sm flex items-center justify-center w-4 h-4 absolute -bottom-1 -right-1 bg-[#0998a8] p-1 rounded-full ">
+            {totalFavorites()}
+          </p>
+        </div>
+        <div className="relative">
+          <ShoppingCart size="26" color="white" className="cursor-pointer" />
+          <p className="text-sm flex items-center justify-center w-4 h-4 absolute -bottom-1 -right-1 bg-[#0998a8] p-1 rounded-full ">
+            {totalQty()}
+          </p>
+        </div>
+        <div>
+          {user?.data ? (
+            <div className="flex gap-2 items-center shadow-custom px-4 py-1">
+              <p>{user?.data?.userName}</p>
+              <img src={user?.data?.avatar} className="w-7 h-7 rounded-full" />
+            </div>
+          ) : (
+            <NavLink
+              to={`/auth`}
+              className={`py-0.5 px-3 bg-[#0998a8] rounded-sm`}
+            >
+              login
+            </NavLink>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default TopMain;
