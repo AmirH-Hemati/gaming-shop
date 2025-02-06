f753d1e97af67ce3dcbad0f31293360a27729587
AmirH-Hemati <amirhosseinhemat9@gmail.com>
2025-02-06 20:16:20 +0330

    change input  style

--- client/src/ui/Input.jsx
+++ client/src/ui/Input.jsx
@@ -1,10 +1,13 @@
-function Input({ type, name, id }) {
+function Input({ type, name, style, id, onChange, value, placeholder }) {
   return (
     <input
       type={type}
       name={name}
+      value={value}
+      onChange={onChange}
+      placeholder={placeholder}
       id={id}
-      className="w-full md:w-1/2 rounded-sm p-3 border-2 border-[#0998a8] outline-none"
+      className={` ${style} outline-none w-full md:w-1/2 bg-white text-black border-2 border-black/30 rounded-sm p-2 `}
     />
   );
 }
