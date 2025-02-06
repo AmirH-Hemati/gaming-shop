5f19ee30deb47da1d3e629fac27f816ff21e00c1
AmirH-Hemati <amirhosseinhemat9@gmail.com>
2025-02-06 12:12:53 +0330

    install jwt-decode for decode token

--- client/src/context/AuthContext.jsx
+++ client/src/context/AuthContext.jsx
@@ -1,15 +1,19 @@
-import { createContext, useContext, useState } from "react";
-
+import { createContext, useContext, useEffect, useState } from "react";
+import jwtDecode from "jwt-decode";
 const authContext = createContext();
 function AuthContextProvider({ children }) {
   const [token, setToken] = useState(localStorage.getItem("token") || "");
   const [role, setRole] = useState("");
+  useEffect(() => {
+    const decode = jwtDecode(token);
+    console.log("decode", decode);
+  }, [token]);
 
   function loginStoredToken(token) {
     localStorage.setItem("token", token);
     setToken(token);
   }
-  
+
   function logout() {
     setToken("");
     localStorage.removeItem("token");
