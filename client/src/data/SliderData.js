7dbf315c6744154e56aa1f9b7ee57660ba630a46
AmirH-Hemati <amirhosseinhemat9@gmail.com>
2025-02-07 12:54:43 +0330

    create slider data in data folder for all data

---
+++ client/src/data/SliderData.js
@@ -0,0 +1,40 @@
+const data = [
+  {
+    image:
+      "https://tse1.mm.bing.net/th?id=OIP.ZOXwa_SUDDwiEJVIIhdOWAHaHa&pid=Api",
+    title: "🎮 کیبورد گیمینگ حرفه‌ای",
+    description: "نورپردازی RGB و عملکرد بی‌نظیر برای تجربه بهتر بازی",
+  },
+  {
+    image:
+      "https://tse4.mm.bing.net/th?id=OIP.u_zO5KTmG0QgzjWAyCltegHaEH&pid=Api",
+    title: "🖱️ ماوس گیمینگ با دقت بالا",
+    description: "سنسور اپتیکال حرفه‌ای با طراحی ارگونومیک",
+  },
+  {
+    image:
+      "https://tse2.mm.bing.net/th?id=OIP.BkgRXVhZnzVSDbCX3ajDWAHaI-&pid=Api",
+    title: "🎧 هدست گیمینگ فراگیر",
+    description: "صدای سه‌بعدی با کیفیت بالا برای ارتباط بهتر در بازی",
+  },
+  {
+    image:
+      "https://tse1.mm.bing.net/th?id=OIP.MQ1iKwU1N-VUDG4qlWxRQwHaEK&pid=Api",
+    title: "🖥️ مانیتور گیمینگ 144Hz",
+    description: "صفحه‌نمایش حرفه‌ای با نرخ تازه‌سازی بالا",
+  },
+  {
+    image:
+      "https://tse1.mm.bing.net/th?id=OIP.dMjQApX8S9OY5MPehXh8twHaE8&pid=Api",
+    title: "⌨️ صندلی گیمینگ ارگونومیک",
+    description: "راحتی بیشتر برای بازی‌های طولانی",
+  },
+  {
+    image:
+      "https://tse1.mm.bing.net/th?id=OIP.z_F8lW_nFeGoNJlqZ_vuZgHaEK&pid=Api",
+    title: "🕹️ کنسول بازی جدید",
+    description: "تجربه نسل جدید بازی‌های ویدیویی",
+  },
+];
+
+export default data;
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import Products from "../features/products/Products";
-
+import sliderData from "../data/SliderData";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/pagination";
@@ -13,44 +13,6 @@ import {
 } from "swiper/modules";
 
 function Home() {
-  const slides = [
-    {
-      image:
-        "https://tse1.mm.bing.net/th?id=OIP.ZOXwa_SUDDwiEJVIIhdOWAHaHa&pid=Api",
-      title: "🎮 کیبورد گیمینگ حرفه‌ای",
-      description: "نورپردازی RGB و عملکرد بی‌نظیر برای تجربه بهتر بازی",
-    },
-    {
-      image:
-        "https://tse4.mm.bing.net/th?id=OIP.u_zO5KTmG0QgzjWAyCltegHaEH&pid=Api",
-      title: "🖱️ ماوس گیمینگ با دقت بالا",
-      description: "سنسور اپتیکال حرفه‌ای با طراحی ارگونومیک",
-    },
-    {
-      image:
-        "https://tse2.mm.bing.net/th?id=OIP.BkgRXVhZnzVSDbCX3ajDWAHaI-&pid=Api",
-      title: "🎧 هدست گیمینگ فراگیر",
-      description: "صدای سه‌بعدی با کیفیت بالا برای ارتباط بهتر در بازی",
-    },
-    {
-      image:
-        "https://tse1.mm.bing.net/th?id=OIP.MQ1iKwU1N-VUDG4qlWxRQwHaEK&pid=Api",
-      title: "🖥️ مانیتور گیمینگ 144Hz",
-      description: "صفحه‌نمایش حرفه‌ای با نرخ تازه‌سازی بالا",
-    },
-    {
-      image:
-        "https://tse1.mm.bing.net/th?id=OIP.dMjQApX8S9OY5MPehXh8twHaE8&pid=Api",
-      title: "⌨️ صندلی گیمینگ ارگونومیک",
-      description: "راحتی بیشتر برای بازی‌های طولانی",
-    },
-    {
-      image:
-        "https://tse1.mm.bing.net/th?id=OIP.z_F8lW_nFeGoNJlqZ_vuZgHaEK&pid=Api",
-      title: "🕹️ کنسول بازی جدید",
-      description: "تجربه نسل جدید بازی‌های ویدیویی",
-    },
-  ];
   return (
     <div className="py-3">
       <Swiper
@@ -75,7 +37,7 @@ function Home() {
         modules={[Navigation, Autoplay, Pagination, EffectCoverflow]}
         className="w-full h-40 md:h-72 shadow-custom rounded-sm"
       >
-        {slides.map((slide, index) => (
+        {sliderData.map((slide, index) => (
           <SwiperSlide key={index} className="md:p-2 rounded-sm">
             <div
               className="relative w-full h-full rounded-sm   bg-cover bg-no-repeat bg-center flex items-center justify-center"
