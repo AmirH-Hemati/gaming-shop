f6244a9c08863907279949802a8feca901d21c35
AmirH-Hemati <amirhosseinhemat9@gmail.com>
2025-02-07 20:29:24 +0330

    create api for updated inforamtion user

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -7,3 +7,19 @@ export async function allUsers(req, res) {
   const users = await User.find({});
   res.json({ message: "ok", data: users });
 }
+
+export async function editUser(req, res) {
+  const { userName, email } = req.body;
+  const updatedUser = { email, userName };
+  if (req.file) {
+    updatedUser.avatar = re.filename;
+  }
+
+  const reslut = await User.findByIdAndUpdate(
+    { _id: req.user._id },
+    updatedUser,
+    { new: true }
+  );
+
+  res.json({ message: "ok", data: reslut });
+}
--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,11 +1,24 @@
 import express from "express";
-import { allUsers, me } from "../controllers/userController.js";
+import { allUsers, editUser, me } from "../controllers/userController.js";
 import { auth } from "../middelware/auth.js";
 import { admin } from "../middelware/admin.js";
+import multer from "multer";
+import path from "path";
 const router = express.Router();
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "images");
+  },
+  filename: (req, file, cb) => {
+    const now = Date.now();
+    cb(null, now + path.extname(file.originalname));
+  },
+});
 
+const upload = multer({ storage: storage });
 router.get("/me", auth, me);
 
 router.get("/users", auth, admin, allUsers);
+router.post("/edit", auth, upload.single("file"), editUser);
 
 export default router;
