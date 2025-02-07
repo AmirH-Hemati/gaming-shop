213ebf5ee75b23513fcba4901fa8c6546e4e8ebf
AmirH-Hemati <amirhosseinhemat9@gmail.com>
2025-02-07 17:32:28 +0330

    create custom hook for get information single product

---
+++ client/src/features/products/useGetProduct.js
@@ -0,0 +1,12 @@
+import { useQuery } from "@tanstack/react-query";
+import { getProduct } from "../../services/apiProducts";
+import { useParams } from "react-router-dom";
+
+export function useGetProduct() {
+  const { id } = useParams();
+  const { data: product, isLoading } = useQuery({
+    queryKey: ["product"],
+    queryFn: () => getProduct(id),
+  });
+  return { product, isLoading };
+}
