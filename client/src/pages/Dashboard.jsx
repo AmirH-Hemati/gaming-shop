e30bf24c3213dcd74bf4b4b145b8db40fcbb627a
AmirH-Hemati <amirhosseinhemat9@gmail.com>
2025-02-08 17:52:04 +0330

    create stst component for show stat in dashboard

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,5 +1,37 @@
+import { ShoppingBag } from "iconsax-react";
+import Stat from "../ui/Stat";
+
 function Dashboard() {
-  return <div>dashboard</div>;
+  return (
+    <div>
+      <div className="flex flex-wrap gap-4">
+        <Stat
+          title={`محصولات`}
+          value={"255"}
+          icon={<ShoppingBag size="32" color="#192938" />}
+          color={`#E0F2FE`}
+        />
+        <Stat
+          title={`کاربران`}
+          value={"255"}
+          icon={<ShoppingBag size="32" color="#192938" />}
+          color={`#DCFCE7`}
+        />
+        <Stat
+          title={`فروش`}
+          value={"255,874"}
+          icon={<ShoppingBag size="32" color="#192938" />}
+          color={`#FEF9C3`}
+        />
+        <Stat
+          title={`محصولات`}
+          value={"255"}
+          icon={<ShoppingBag size="32" color="#192938" />}
+          color={`#E0F2FE`}
+        />
+      </div>
+    </div>
+  );
 }
 
 export default Dashboard;
