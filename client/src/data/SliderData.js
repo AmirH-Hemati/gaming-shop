f606eea63fe7c28d21f2b29578baaa38e07f65e4
AmirH-Hemati <amirhosseinhemat9@gmail.com>
2025-02-08 13:54:20 +0330

    change style and change breakpoint slider

--- client/src/data/SliderData.js
+++ client/src/data/SliderData.js
@@ -1,39 +1,12 @@
 const data = [
   {
-    image:
-      "https://tse1.mm.bing.net/th?id=OIP.ZOXwa_SUDDwiEJVIIhdOWAHaHa&pid=Api",
-    title: "🎮 کیبورد گیمینگ حرفه‌ای",
-    description: "نورپردازی RGB و عملکرد بی‌نظیر برای تجربه بهتر بازی",
+    image: "https://gamingshop98.com/wp-content/uploads/2025/01/وب.webp",
   },
   {
-    image:
-      "https://tse4.mm.bing.net/th?id=OIP.u_zO5KTmG0QgzjWAyCltegHaEH&pid=Api",
-    title: "🖱️ ماوس گیمینگ با دقت بالا",
-    description: "سنسور اپتیکال حرفه‌ای با طراحی ارگونومیک",
+    image: "https://gamingshop98.com/wp-content/uploads/2025/01/رد-سایت.webp",
   },
   {
-    image:
-      "https://tse2.mm.bing.net/th?id=OIP.BkgRXVhZnzVSDbCX3ajDWAHaI-&pid=Api",
-    title: "🎧 هدست گیمینگ فراگیر",
-    description: "صدای سه‌بعدی با کیفیت بالا برای ارتباط بهتر در بازی",
-  },
-  {
-    image:
-      "https://tse1.mm.bing.net/th?id=OIP.MQ1iKwU1N-VUDG4qlWxRQwHaEK&pid=Api",
-    title: "🖥️ مانیتور گیمینگ 144Hz",
-    description: "صفحه‌نمایش حرفه‌ای با نرخ تازه‌سازی بالا",
-  },
-  {
-    image:
-      "https://tse1.mm.bing.net/th?id=OIP.dMjQApX8S9OY5MPehXh8twHaE8&pid=Api",
-    title: "⌨️ صندلی گیمینگ ارگونومیک",
-    description: "راحتی بیشتر برای بازی‌های طولانی",
-  },
-  {
-    image:
-      "https://tse1.mm.bing.net/th?id=OIP.z_F8lW_nFeGoNJlqZ_vuZgHaEK&pid=Api",
-    title: "🕹️ کنسول بازی جدید",
-    description: "تجربه نسل جدید بازی‌های ویدیویی",
+    image: "https://gamingshop98.com/wp-content/uploads/2025/01/جانبی.webp",
   },
 ];
 
--- client/src/ui/Slider.jsx
+++ client/src/ui/Slider.jsx
@@ -22,14 +22,6 @@ function Slider() {
         delay: 2500,
         disableOnInteraction: false,
       }}
-      breakpoints={{
-        320: {
-          slidesPerView: 1,
-        },
-        768: {
-          slidesPerView: 2,
-        },
-      }}
       coverflowEffect={{
         rotate: 35,
         stretch: 200,
@@ -41,19 +33,13 @@ function Slider() {
       className="w-full  h-40 md:h-60 shadow-custom rounded-sm"
     >
       {sliderData.map((slide, index) => (
-        <SwiperSlide key={index} className="md:p-2 rounded-sm">
-          <div
-            className="relative w-full h-full rounded-sm   bg-cover bg-no-repeat bg-center flex items-center justify-center"
-            style={{ backgroundImage: `url(${slide.image})` }}
-          >
-            <div className="absolute inset-0 bg-black/50"></div>
-            <div className="relative z-10 text-white text-center p-6">
-              <h2 className="text-sm   font-bold mb-2 text-white/80">
-                {slide.title}
-              </h2>
-              <p className="text-sm text-white/50">{slide.description}</p>
-            </div>
-          </div>
+        <SwiperSlide key={index} className=" rounded-sm">
+          <div className="absolute inset-0 bg-black/20"></div>
+          <img
+            src={slide.image}
+            alt=""
+            className="w-full h-full object-cover"
+          />
         </SwiperSlide>
       ))}
     </Swiper>
